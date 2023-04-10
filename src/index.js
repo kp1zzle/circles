4f27109d9568ed6597ad620d49b52656ea33ff95
Kieran Sagar Parikh <kieran@parikh.ca>
2023-04-10 11:48:58 -0400

    fix device motion permissions

--- src/index.js
+++ src/index.js
@@ -404,18 +404,3 @@ let sketch = (s) => {
 }
 
 const P5 = new p5(sketch);
-
-function requestDeviceMotionPermission() {
-    // feature detect
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-        DeviceMotionEvent.requestPermission()
-            .then(permissionState => {
-                if (permissionState === 'granted') {
-                    window.addEventListener('devicemotion', () => {});
-                }
-            })
-            .catch(console.error);
-    } else {
-        // handle regular non iOS 13+ devices
-    }
-}
